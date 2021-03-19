3ba838a9d62ab25f12778ae7fe9915b57a4f7879
Molinari <julieta.molinari@accenture.com>
2021-03-18 22:36:42 -0300

    Primera claseb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeraClase2';
+  public edad1:number=0;
+  public edad2:number=0;
+  public promedio:number=0;
+  public mostrar:boolean =false;
+
+  public calcularPromedio(): void {
+    this.promedio = (this.edad1 + this.edad2) /2;
+    this.mostrar=true;
+  }
+
+  public Limpiar() : void{
+    this.edad1 = 0;
+    this.edad2 = 0; 
+    this.mostrar = false;
+
+  }
 }
